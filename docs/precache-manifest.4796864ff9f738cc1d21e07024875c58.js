98ba777e927a76d67ad62b4395673aee67a3d628
Kevin Zambrano <kev_1392@hotmail.com>
2020-08-06 14:14:34 -0500

    docs agregado

---
+++ docs/precache-manifest.4796864ff9f738cc1d21e07024875c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f1f30c8106d8bcd2efa5bd9bf14638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3fc30dd78cdeea32d20",
+    "url": "/static/css/main.1becb113.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3fc30dd78cdeea32d20",
+    "url": "/static/js/main.a8353ddf.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
